c74212e3784383dc1334745465ed8cf880c359d5
Ubuntu <ubuntu@ip-172-31-73-164.ec2.internal>
2024-08-27 23:51:18 +0000

    dot products

--- build.rs
+++ build.rs
@@ -1,5 +1,19 @@
+use std::process::Command;
+
 fn main() {
+    // Check for AVX512 support
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("lscpu | grep avx512")
+        .output()
+        .expect("Failed to execute command");
+
+    if !output.status.success() || output.stdout.is_empty() {
+        panic!("NO AVX_512");
+    }
     cc::Build::new()
         .file("src/avx512montmul.S")
+        .file("src/innerproduct256.S")
+        .file("src/modip256_mont.S")
         .compile("avx512montmul");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::hint::black_box;
-use ark_bn254::Fq;
+use ark_bn254::{Fq, Fr};
 use ark_ff::PrimeField;
 use ark_std::Zero;
 use vectorized_fields::{read_vec_from_file, save_vec_to_file};
@@ -10,17 +10,115 @@ use rand::SeedableRng;
 
 extern "C" {
     fn avx512montmul(z: *mut u32, x: *const u32, y: *const u32, m: *const u64);
+    fn modip256_mont(z: *mut u64, x: *const u64, y: *const u64, xy_len: u32, m: *const u64);
 }
 
 // Base Field
-const FP: [u64; 9] = [
-    0xD87CFD47, 0x3C208C16, 0x6871CA8D, 0x97816A91, 0x8181585D, 0xB85045B6, 0xE131A029, 0x30644E72, 0xE4866389,
+// const FP: [u64; 9] = [
+//     0xD87CFD47, 0x3C208C16, 0x6871CA8D, 0x97816A91, 0x8181585D, 0xB85045B6, 0xE131A029, 0x30644E72, 0xE4866389,
+// ];
+
+const FP: [u64; 6] = [
+    // Modulus: 0x30644E72E131A029B85045B68181585D97816A916871CA8D3C208C16D87CFD47
+    0x3C208C16D87CFD47, 0x97816A916871CA8D, 0xB85045B68181585D, 0x30644E72E131A029,
+
+    // Negative inverse of modulus: 0xF57A22B791888C6BD8AFCBD01833DA809EDE7D651ECA6AC987D20782E4866389
+    // The least significant 64-bit word only:
+    0x87D20782E4866389,
+
+    // For Barrett: mu = 2^288 / m
+    0x000000054A474626
+];
+
+const FR: [u64; 6] = [
+    // Modulus: 0x30644E72E131A029B85045B68181585D2833E84879B9709143E1F593F0000001
+    0x43e1f593f0000001, 0x2833e84879b97091, 0xb85045b68181585d, 0x30644e72e131a029,
+
+    // Negative inverse of modulus: 0x40D019D832A0FCE8AEF9B39374A81A7665DE1528CB3816E9C2E1F593EFFFFFFF
+    // The least significant 64-bit word only:
+    0xc2e1f593efffffff,
+
+    // For Barrett: mu = 2^288 / m
+    0x000000054A474626
 ];
 
 fn main() {
     // test_file();
     // test_memory();
-    test_memory_par();
+    // test_memory_par();
+    test_inner_prod();
+}
+
+fn test_inner_prod() {
+    const NUM_OPS: usize = 8 * 128 * 64_000;
+    // const NUM_OPS: usize = 4;
+
+    let x: Vec<Fr> = rand_vec(NUM_OPS);
+    let y: Vec<Fr> = rand_vec(NUM_OPS);
+    // let x: Vec<Fr> = vec![Fr::from(2),  Fr::from(2), Fr::from(2), Fr::from(2)];
+    // let y: Vec<Fr> = vec![Fr::from(1), Fr::from(1), Fr::from(1), Fr::from(1)];
+    let ark_x = x.clone();
+    let ark_y = y.clone();
+    let mut ark_z: Fr = Fr::zero();
+
+    let chunk_size = NUM_OPS / 10;
+
+    let ark_start = std::time::Instant::now();
+
+    ark_z = ark_x.par_chunks(chunk_size)
+        .zip(ark_y.par_chunks(chunk_size))
+        .map(|(chunk_x, chunk_y)| {
+            chunk_x.iter().zip(chunk_y.iter())
+                .map(|(&x, &y)| x * y)
+                .sum::<Fr>()
+        })
+        .sum();
+    black_box(ark_z);
+    let elapsed_ark = ark_start.elapsed();
+
+    let other_x = x;
+    let other_y = y;
+    let mut other_z = Fr::zero();
+
+
+    let simd_start = std::time::Instant::now();
+    use rayon::prelude::*;
+
+
+    let other_z: Fr = other_x.par_chunks(chunk_size)
+        .zip(other_y.par_chunks(chunk_size))
+        .map(|(chunk_x, chunk_y)| {
+            let simd_x = chunk_x.as_ptr() as *const u64;
+            let simd_y = chunk_y.as_ptr() as *const u64;
+            let mut collect = Fr::zero();
+            let mut simd_z = collect.0.0.as_mut_ptr() as *mut u64;
+            let simd_len: u32 = chunk_x.len().try_into().unwrap();
+
+            unsafe {
+                modip256_mont(
+                    simd_z,
+                    simd_x,
+                    simd_y,
+                    simd_len,
+                    FR.as_ptr()
+                );
+            }
+            collect
+        })
+        .sum::<Fr>();
+
+    // other_z = intermediate_zs.into_iter().sum();
+    black_box(&other_z);
+    let elapsed_simd = simd_start.elapsed();
+
+    println!("Ark: {elapsed_ark:?}");
+    println!("Simd: {elapsed_simd:?}");
+    let multiple = elapsed_ark.as_secs_f64() / elapsed_simd.as_secs_f64();
+    println!("Ark {:.2}x Slower", multiple);
+    let percentage = 100.0 / multiple;
+    println!("Simd is {:.2}% of Ark", percentage);
+
+    assert_eq!(other_z, ark_z);
 }
 
 /// Version serializes to a file after generating. Takes substantially longer to run due to disk ops
