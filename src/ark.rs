2c712f2a4031b89864e239e3c3d8f94a6fcc3916
EC2 Default User <ec2-user@ip-172-31-45-3.ec2.internal>
2024-08-28 16:45:07 +0000

    add, sub, sum -- sum not working

---
+++ src/ark.rs
@@ -0,0 +1,108 @@
+use ark_ff::PrimeField;
+use rayon::prelude::*;
+
+pub fn ark_batch_mul<F: PrimeField>(x: &[F], y: &[F], z: &mut [F]) {
+    let len = x.len();
+    assert_eq!(len, y.len());
+    assert_eq!(len, z.len());
+
+    for i in 0..len {
+        z[i] = x[i] * y[i];
+    }
+}
+
+pub fn ark_batch_mul_par<F: PrimeField>(x: &[F], y: &[F], z: &mut [F]) {
+    let len = x.len();
+    assert_eq!(len, y.len());
+    assert_eq!(len, z.len());
+
+    x.par_iter()
+        .zip(y.par_iter())
+        .zip(z.par_iter_mut())
+        .for_each(|((xi, yi), zi)| {
+            *zi = *xi * *yi;
+        });
+}
+
+pub fn ark_inner_product<F: PrimeField>(x: &[F], y: &[F]) -> F {
+    let len = x.len();
+    assert_eq!(len, y.len());
+
+    let chunk_size = len / rayon::current_num_threads();
+
+    x.par_chunks(chunk_size)
+        .zip(y.par_chunks(chunk_size))
+        .map(|(chunk_x, chunk_y)| {
+            chunk_x
+                .iter()
+                .zip(chunk_y.iter())
+                .map(|(&x, &y)| x * y)
+                .sum::<F>()
+        })
+        .sum()
+}
+
+pub fn ark_batch_sum<F: PrimeField>(x: &[F]) -> F {
+    let len = x.len();
+
+    let mut z = F::zero();
+    for i in 0..len {
+        z += x[i];
+    }
+    z
+}
+
+pub fn ark_batch_sum_par<F: PrimeField>(x: &[F], z: &mut [F]) {
+    let len = x.len();
+    assert_eq!(len, z.len());
+
+    x.par_iter().zip(z.par_iter_mut()).for_each(|(xi, zi)| {
+        *zi = *xi + *zi;
+    });
+}
+
+pub fn ark_batch_add<F: PrimeField>(x: &[F], y: &[F], z: &mut [F]) {
+    let len = x.len();
+    assert_eq!(len, y.len());
+    assert_eq!(len, z.len());
+
+    for i in 0..len {
+        z[i] = x[i] + y[i];
+    }
+}
+
+pub fn ark_batch_add_par<F: PrimeField>(x: &[F], y: &[F], z: &mut [F]) {
+    let len = x.len();
+    assert_eq!(len, y.len());
+    assert_eq!(len, z.len());
+
+    x.par_iter()
+        .zip(y.par_iter())
+        .zip(z.par_iter_mut())
+        .for_each(|((xi, yi), zi)| {
+            *zi = *xi + *yi;
+        });
+}
+
+pub fn ark_batch_sub<F: PrimeField>(x: &[F], y: &[F], z: &mut [F]) {
+    let len = x.len();
+    assert_eq!(len, y.len());
+    assert_eq!(len, z.len());
+
+    for i in 0..len {
+        z[i] = x[i] - y[i];
+    }
+}
+
+pub fn ark_batch_sub_par<F: PrimeField>(x: &[F], y: &[F], z: &mut [F]) {
+    let len = x.len();
+    assert_eq!(len, y.len());
+    assert_eq!(len, z.len());
+
+    x.par_iter()
+        .zip(y.par_iter())
+        .zip(z.par_iter_mut())
+        .for_each(|((xi, yi), zi)| {
+            *zi = *xi - *yi;
+        });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use ark_std::Zero;
 
 use rayon::prelude::*;
 
+mod ark;
 mod assembly;
 pub mod benchmarks;
 mod constants;
@@ -76,12 +77,9 @@ pub fn sub_vec_par_bn254(x: &[Fr], y: &[Fr], z: &mut [Fr]) {
         });
 }
 
-pub fn sum_vec_bn254(x: &[Fr], y: &[Fr]) -> Fr {
-    assert_eq!(x.len(), y.len());
-
+pub fn sum_vec_bn254(x: &[Fr]) -> Fr {
     let mut result = Fr::zero();
     let simd_x = x.as_ptr() as *const u64;
-    let simd_y = y.as_ptr() as *const u64;
     let simd_result = result.0 .0.as_mut_ptr() as *mut u64;
 
     unsafe {
@@ -91,24 +89,16 @@ pub fn sum_vec_bn254(x: &[Fr], y: &[Fr]) -> Fr {
             x.len() as u32,
             constants::BN254_FR.as_ptr(),
         );
-        modsum256(
-            simd_result,
-            simd_y,
-            y.len() as u32,
-            constants::BN254_FR.as_ptr(),
-        );
     }
 
     result
 }
 
-pub fn sum_vec_par_bn254(x: &[Fr], y: &[Fr]) -> Fr {
-    assert_eq!(x.len(), y.len());
+pub fn sum_vec_par_bn254(x: &[Fr]) -> Fr {
     let chunk_size = x.len() / rayon::current_num_threads();
 
     x.par_chunks(chunk_size)
-        .zip(y.par_chunks(chunk_size))
-        .map(|(chunk_x, chunk_y)| sum_vec_bn254(chunk_x, chunk_y))
+        .map(|chunk_x| sum_vec_bn254(chunk_x))
         .sum::<Fr>()
 }
 
@@ -183,6 +173,7 @@ pub fn inner_product_par_bn254(x: &[Fr], y: &[Fr]) -> Fr {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::ark::*;
     use crate::utils::rand_vec;
     use ark_std::Zero;
 
@@ -211,5 +202,110 @@ mod tests {
         let z = inner_product_bn254(&x, &y);
         let par_z = inner_product_par_bn254(&x, &y);
         assert_eq!(z, par_z);
+    }
+
+    #[test]
+    fn parallel_parity_vec_add() {
+        const NUM_OPS: usize = 10_000;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+        let y: Vec<Fr> = rand_vec(NUM_OPS);
+        let mut z: Vec<Fr> = vec![Fr::zero(); NUM_OPS];
+
+        let x_par = x.clone();
+        let y_par = y.clone();
+        let mut z_par = z.clone();
+
+        add_vec_bn254(&x, &y, &mut z);
+        add_vec_par_bn254(&x_par, &y_par, &mut z_par);
+        assert_eq!(z, z_par);
+    }
+
+    #[test]
+    fn parallel_parity_vec_sub() {
+        const NUM_OPS: usize = 10_000;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+        let y: Vec<Fr> = rand_vec(NUM_OPS);
+        let mut z: Vec<Fr> = vec![Fr::zero(); NUM_OPS];
+
+        let x_par = x.clone();
+        let y_par = y.clone();
+        let mut z_par = z.clone();
+
+        sub_vec_bn254(&x, &y, &mut z);
+        sub_vec_par_bn254(&x_par, &y_par, &mut z_par);
+        assert_eq!(z, z_par);
+    }
+
+    #[test]
+    fn parallel_parity_vec_sum() {
+        const NUM_OPS: usize = 10_000;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+
+        let x_par = x.clone();
+
+        let z = sum_vec_bn254(&x);
+        let z_par = sum_vec_par_bn254(&x_par);
+        assert_eq!(z, z_par);
+    }
+
+    #[test]
+    fn parity_ark_mul() {
+        const NUM_OPS: usize = 5;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+        let y: Vec<Fr> = rand_vec(NUM_OPS);
+        let mut z: Vec<Fr> = vec![Fr::zero(); NUM_OPS];
+        let mut ark_z = z.clone();
+
+        ark_batch_mul(&x, &y, &mut ark_z);
+        mul_vec_bn254(&x, &y, &mut z);
+        assert_eq!(z, ark_z);
+    }
+
+    #[test]
+    fn parity_ark_add() {
+        const NUM_OPS: usize = 5;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+        let y: Vec<Fr> = rand_vec(NUM_OPS);
+        let mut z: Vec<Fr> = vec![Fr::zero(); NUM_OPS];
+        let mut ark_z = z.clone();
+
+        ark_batch_add(&x, &y, &mut ark_z);
+        add_vec_bn254(&x, &y, &mut z);
+        assert_eq!(z, ark_z);
+    }
+
+    #[test]
+    fn parity_ark_sub() {
+        const NUM_OPS: usize = 5;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+        let y: Vec<Fr> = rand_vec(NUM_OPS);
+        let mut z: Vec<Fr> = vec![Fr::zero(); NUM_OPS];
+        let mut ark_z = z.clone();
+
+        ark_batch_sub(&x, &y, &mut ark_z);
+        sub_vec_bn254(&x, &y, &mut z);
+        assert_eq!(z, ark_z);
+    }
+
+    #[test]
+    fn parity_ark_inner_product() {
+        const NUM_OPS: usize = 500;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+        let y: Vec<Fr> = rand_vec(NUM_OPS);
+
+        let ark_z = ark_inner_product(&x, &y);
+        let z = inner_product_bn254(&x, &y);
+        assert_eq!(z, ark_z);
+    }
+
+    #[test]
+    fn parity_ark_sum() {
+        const NUM_OPS: usize = 5;
+        let x: Vec<Fr> = rand_vec(NUM_OPS);
+        let ark_x = x.clone();
+
+        let ark_z = ark_batch_sum(&ark_x);
+        let z = sum_vec_bn254(&x);
+        assert_eq!(z, ark_z);
     }
 }
