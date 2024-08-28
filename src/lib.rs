1c7f0b0fcf61c76314efdc601481d4a167e93c42
EC2 Default User <ec2-user@ip-172-31-45-3.ec2.internal>
2024-08-28 16:06:52 +0000

    cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -84,39 +84,6 @@ pub fn inner_product_par_bn254(x: &[Fr], y: &[Fr]) -> Fr {
         .sum::<Fr>()
 }
 
-pub fn save_vec_to_file<F: PrimeField>(file_name: &str, vec: &[F]) {
-    let file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true) // This ensures the file is always replaced if it exists
-        .open(file_name)
-        .expect("Unable to create or open file");
-    let mut writer = BufWriter::new(file);
-
-    for element in vec {
-        element
-            .serialize_compressed(&mut writer)
-            .expect("should serialize");
-    }
-}
-
-pub fn read_vec_from_file<F: PrimeField>(file_name: &str) -> Vec<F> {
-    let file = File::open(file_name).expect("Unable to open file");
-    let mut reader = BufReader::new(file);
-    let mut buffer = Vec::new();
-
-    reader
-        .read_to_end(&mut buffer)
-        .expect("Unable to read data");
-
-    let mut deserializer = buffer.as_slice();
-    let mut vec = Vec::new();
-    while let Ok(element) = F::deserialize_compressed(&mut deserializer) {
-        vec.push(element);
-    }
-    vec
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use ark_bn254::{Fq, Fr};
-use ark_ff::PrimeField;
-use ark_std::Zero;
-use rand::SeedableRng;
-use rand_chacha::ChaCha8Rng;
-use rayon::prelude::*;
-use std::hint::black_box;
-use vectorized_fields::{read_vec_from_file, save_vec_to_file};
-
 use vectorized_fields::benchmarks;
 
 fn main() {
